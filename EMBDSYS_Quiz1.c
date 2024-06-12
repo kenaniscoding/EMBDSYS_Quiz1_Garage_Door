ffc6aba04123d0a3ed056ec5578e00864c82bc59
Kenan Banal <112913035+kenaniscoding@users.noreply.github.com>
2024-06-12 10:31:32 +0800

    Add files via upload

--- EMBDSYS_Quiz1.c
+++ EMBDSYS_Quiz1.c
@@ -7,25 +7,25 @@ void main()
   PORTC = 0x00;
   //INPUT
   TRISB = 0b00001111;
-//S1=door open clockWise
-//S2=door stop for 30 seconds
-//S3=door close counterClockWise
-//S4=stop motor from rotating
+//S1=door open clockWise, on buzzer, and onnly on led at C0
+//S2=door stop DC motor, off buzzer, and only on led at C1
+//S3=door close counterClockWise, on buzzer, and only on led at C2
+//S4=stop motor from rotating, off buzzer, and only on led at C3
 
 while(1){
-if (PORTB.F3==1){
+if (PORTB.F3==1){//S1
    //dc motor off
    PORTD.F2=0;
    PORTD.F1=0;
    //buzzer off
    PORTC.F0=0;
-    //LED
+   //LED
    PORTC.F4=1;
    PORTC.F1=0;
    PORTC.F2=0;
    PORTC.F3=0;
 }  else{
-if(PORTB.F0==1 && PORTB.F1==0 && PORTB.F2==0){
+if(PORTB.F0==1 && PORTB.F1==0 && PORTB.F2==0){//S2
  //S1=door open clockWise
    PORTD.F2=1;
    PORTD.F1=1;
@@ -37,7 +37,7 @@ if(PORTB.F0==1 && PORTB.F1==0 && PORTB.F2==0){
    PORTC.F2=0;
    PORTC.F3=0;
    //delay_ms(1000);
- }else if (PORTB.F0==1 && PORTB.F1==1 && PORTB.F2==0){
+ }else if (PORTB.F0==1 && PORTB.F1==1 && PORTB.F2==0){ //S3
  //S2 door stop
    PORTD.F2=0;
    PORTD.F1=0;
@@ -48,7 +48,7 @@ if(PORTB.F0==1 && PORTB.F1==0 && PORTB.F2==0){
    PORTC.F1=0;
    PORTC.F2=1;
    PORTC.F3=0;
- }else if (PORTB.F0==1 && PORTB.F1==1 && PORTB.F2==1){
+ }else if (PORTB.F0==1 && PORTB.F1==1 && PORTB.F2==1){ //S4
   //S1=door open counterClockWise
  PORTD.F2=1;
  PORTD.F1=0;
